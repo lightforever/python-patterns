89937afbc046f2e05f4969619ce1a3355f303d94
Vasiliy Kirstia <kirstiavasilii@gmail.com>
2019-02-25 08:07:00 +0200

    Added set_name implementation for descriptor

--- lazy_evaluation/vasiliy/lazy_fields.py
+++ lazy_evaluation/vasiliy/lazy_fields.py
@@ -2,12 +2,15 @@ import math
 
 
 class SquaredRootOfFive:
+    def __set_name__(self, owner, name):
+        self.name = name
+
     def __set__(self, instance, value):
         raise AttributeError()
 
     def __get__(self, instance, owner):
         if not hasattr(self, 'sqrt_5'):
-            print('---lazy calculations---')
+            print(f'---lazy calculations of {self.name}---')
             self.sqrt_5 = math.sqrt(5)
 
         return self.sqrt_5
