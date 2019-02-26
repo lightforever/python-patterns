108084321943f45a2841ebbb12d840f6483d2607
Evgeny Semyonov <lightsanweb@yandex.ru>
2019-02-26 13:58:34 +0300

    flyweight

--- flyweight/lightforever/main.py
+++ flyweight/lightforever/main.py
@@ -1,26 +1,29 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from copy import copy
 
 class FileChange:
     def __init__(self, folder:str, name:str, date_created: datetime, date_modified: datetime):
         self.folder = folder
         self.name = name
-        self.date_created = date_created
-        self.date_modified = date_modified
+        self.date_created = copy(date_created)
+        self.date_created2 = copy(date_created)
+        self.date_modified = copy(date_modified)
 
 class File:
-    def __init__(self, folder: str, name: str, date_created: datetime):
+    def __init__(self, folder: str, name: str, date_created: datetime, date_created2: datetime):
         self.folder = folder
         self.name = name
         self.date_created = date_created
+        self.date_created2 = date_created2
 
     def key(self):
-        return hash(f'folder={self.folder},name={self.name},date_created={self.date_created}')
+        return hash(f'folder={self.folder},name={self.name},date_created={self.date_created},date_created2={self.date_created2}')
 
 class FileChangeFlyweight:
     _files = dict()
 
     def __init__(self, folder: str, name: str, date_created: datetime, date_modified: datetime):
-        file = File(folder, name, date_created)
+        file = File(folder, name, copy(date_created), copy(date_created))
         if file.key() not in FileChangeFlyweight._files:
             FileChangeFlyweight._files[file.key()] = file
         else:
@@ -32,12 +35,14 @@ class FileChangeFlyweight:
 if __name__=='__main__':
     objects = []
     dt = datetime.now()
+    modified = datetime.now()+ timedelta(days=10)
     folder = '/home'
     name = 'log.txt'
-    for i in range(10**2):
-        objects.append(FileChange(folder, name, dt, dt))
+    for i in range(10**6):
+        objects.append(FileChange(folder, name, dt, modified))
 
     objects = []
-    for i in range(10**2):
-        objects.append(FileChangeFlyweight(folder, name, dt, dt))
+    for i in range(10**6):
+        objects.append(FileChangeFlyweight(folder, name, dt, modified))
+
 
